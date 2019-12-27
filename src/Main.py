6f5c1510813fc56a5844ca30c60e09014592c41a
Tiberwood <cristobal@rhinostudio.cl>
2019-12-27 00:27:41 -0300

    generateReportData method fixed

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,12 @@
 from classes import Student
 import sys
 
-def createReport(self, path, report_array):
+def createReport(path, report_array):
     file = open(path, 'w')
+    file = open(path, 'a')
     for line in report_array:
         file.write(line)
+    import pdb;pdb.set_trace()
     file.close()
         
 def generateReportData(students_array, presence_array):
@@ -17,13 +19,13 @@ def generateReportData(students_array, presence_array):
                 student = Student(presence[1], presence[3], presence[4], presence[5])
                 total_days += 1
                 minutes = student.getMinutesPerDay() 
-                if minutes < 5:
+                if minutes > 5:
                     total_minutes += minutes
                 continue
         if total_minutes == 0:
-            report_array.append(student_name[1] + ': ' + str(total_minutes) + ' minutes')
+            report_array.append(student_name[1] + ': ' + str(total_minutes) + ' minutes\n')
         days = ' days' if total_days > 1 else ' day'
-        report_array.append(student_name[1] + ': ' + str(total_minutes) + ' minutes in ' + str(total_days) + days)
+        report_array.append(student_name[1] + ': ' + str(total_minutes) + ' minutes in ' + str(total_days) + days + '\n')
     return report_array
 
 
@@ -31,8 +33,11 @@ if __name__ == '__main__':
     filename = str(sys.argv[1])
     report_array = []
     report_file = 'output.txt'
+    # read the file from argument
     with open(filename, 'r') as file:
+        #read the file line by line
         lines = file.readlines()
+        #arrays for each command
         students_array = []
         presence_array = []
         for line in lines:
@@ -46,9 +51,10 @@ if __name__ == '__main__':
                 #with presence command
                 presence_array.append(values_array)
                 continue
-        #sort the array
+        #sort the arrays
         students_array.sort()
         presence_array.sort()
         report_data = generateReportData(students_array, presence_array)
         createReport(report_file, report_data)
+        
         print('=D')
